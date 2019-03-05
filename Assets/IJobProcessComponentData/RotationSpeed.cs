97eea4bebc43fa842a63344964d78c6775ea4a2b
fh9501 <1119369173@qq.com>
2019-03-05 22:39:10 +0800

    IJobProcessComponentData

---
+++ Assets/IJobProcessComponentData/RotationSpeed.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.Entities;
+
+namespace IJobProcessComponentData
+{
+    [Serializable]
+    public struct RotationSpeed : IComponentData
+    {
+        public float radiansPerSecond;
+    }
+}
---
+++ Assets/IJobProcessComponentData/RotationSpeedSystem.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace IJobProcessComponentData
+{
+    public class RotationSpeedSystem : JobComponentSystem
+    {
+        [BurstCompile]
+        private struct RotationSpeedJob : IJobProcessComponentData<Rotation, RotationSpeed>
+        {
+            public float DeltaTime;
+
+            public void Execute(ref Rotation rotation, [ReadOnly] ref RotationSpeed rotationSpeed)
+            {
+                rotation.Value = math.mul(rotation.Value,
+                    quaternion.AxisAngle(math.up(), DeltaTime * rotationSpeed.radiansPerSecond));
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var job = new RotationSpeedJob
+            {
+                DeltaTime = Time.deltaTime
+            };
+            return job.Schedule(this, inputDeps);
+        }
+    }
+}
