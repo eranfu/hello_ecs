3c9f6805dc64ea4bae0edf1cced6eb1c767936e1
fh9501 <1119369173@qq.com>
2019-03-09 21:10:33 +0800

    SpawnFromMonoBehaviour

---
+++ Assets/SpawnFromMonoBehaviour/SpawnFromMonoBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace SpawnFromMonoBehaviour
+{
+    public class SpawnFromMonoBehaviour : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private int countX;
+        [SerializeField] private int countY;
+
+        private void Start()
+        {
+            Entity entityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(prefab, World.Active);
+            EntityManager entityManager = World.Active.EntityManager;
+            for (var y = 0; y < countY; y++)
+            {
+                for (var x = 0; x < countX; x++)
+                {
+                    Entity instance = entityManager.Instantiate(entityPrefab);
+                    float3 position = transform.TransformPoint(new float3(x * 1.3f,
+                        noise.cnoise(new float2(x, y) * 0.21f) * 2, y * 1.3f));
+                    entityManager.SetComponentData(instance, new Translation {Value = position});
+                }
+            }
+        }
+    }
+}
